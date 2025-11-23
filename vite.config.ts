de55882dd53983b1f1098349e0dc960b366f7c7a
Raul andres Gutierrez <raulandresgutierrez@MacBook-Air-de-Raul.local>
2025-11-23 02:04:17 -0300

    Fix blank page: add base path to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/event-pix/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
