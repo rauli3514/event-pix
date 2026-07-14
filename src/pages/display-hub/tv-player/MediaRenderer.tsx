e46c9f581b3468b162e25456ba97efcea2075090
Raul Gutierrez <rauli3514@gmail.com>
2026-07-14 19:17:21 -0300

    fix(tv): remove unused React import in MediaRenderer

--- src/pages/display-hub/tv-player/MediaRenderer.tsx
+++ src/pages/display-hub/tv-player/MediaRenderer.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ErrorInfo, ReactNode } from 'react';
+import { Component, ErrorInfo, ReactNode } from 'react';
 import { PlayerRenderer } from '@/components/display/PlayerRenderer';
 import { CampaignItem } from '@/types/display';
 
